edc71e18da12439f0056be0f78628bceaea8b392
widnyana <wid@widnyana.web.id>
2020-02-28 13:58:08 +0700

    change tag
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- go-builder/go1.13-alpine/build.sh
+++ go-builder/go1.13-alpine/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build --no-cache --rm -t "widnyana/go-builder" -f Dockerfile .
+DOCKER_BUILDKIT=1 docker build --no-cache --rm -t "widnyana/go-builder:1.13-alpine" -f Dockerfile .
 docker push widnyana/go-builder
