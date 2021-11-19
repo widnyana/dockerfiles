1b1686c6f11b4b671cd7b3c77f9cdc9e88deb7ad
widnyana <wid@widnyana.web.id>
2021-11-19 11:42:21 +0700

    feat:(go1.13) add make
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- go-builder/go1.13-alpine/build.sh
+++ go-builder/go1.13-alpine/build.sh
@@ -5,8 +5,7 @@ TAGNAME="widnyana/go-builder:1.13-alpine"
 
 DOCKER_BUILDKIT=1 docker build \
     --pull \
-    --no-cache \
-    --rm \
     -t "${TAGNAME}" \
-    -f Dockerfile . \
-  && docker push ${TAGNAME}
+    -f Dockerfile .
+
+docker push ${TAGNAME}
