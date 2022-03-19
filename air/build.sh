895688dae90ea0ffa726ebbba128fc4e4d8b5404
widnyana <wid@widnyana.web.id>
2022-03-19 21:29:27 +0700

    fix: revise wrong variable placement
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- air/build.sh
+++ air/build.sh
@@ -4,9 +4,9 @@ VERSION=${1:-"master"}
 DOCKER_BUILDKIT=1 
 
 if [[ $VERSION == "master" ]]; then
-  TAGNAME="widnyana/air:${VERSION}"
-else
   TAGNAME="widnyana/air:latest"
+else
+  TAGNAME="widnyana/air:${VERSION}"
 fi
 
 
