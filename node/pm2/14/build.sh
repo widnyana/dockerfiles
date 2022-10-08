f13950f33d7aedb478a8d275e9ec07ccccf565f2
widnyana <wid@widnyana.web.id>
2022-10-08 16:18:22 +0700

    feat: add PM2 on node 16
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- node/pm2/14/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TAGNAME="widnyana/pm2:14-alpine"
-
-DOCKER_BUILDKIT=1 docker build \
-    -t "${TAGNAME}" \
-    -f Dockerfile . \
-  && docker push ${TAGNAME} \
-  && docker rmi ${TAGNAME}
